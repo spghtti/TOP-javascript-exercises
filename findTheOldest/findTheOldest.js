83bd29f31f21f1531dd95384af8a627edf7d1762
Matt Daniele <2266075+spghtti@users.noreply.github.com>
2020-12-14 14:59:38 -0500

    Adding solutions for findTheOldest and getTheTitles

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    const oldest = people.sort(function(a, b) {
+        if (!a.hasOwnProperty('yearOfDeath')) {
+            const lastGuy = currentYear - a.yearOfBirth;
+            const nextGuy = b.yearOfDeath - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
 
+        } else if (!b.hasOwnProperty('yearOfDeath')) {
+            const lastGuy = a.yearOfDeath - a.yearOfBirth;
+            const nextGuy = currentYear - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
+            
+        } else {
+            const lastGuy = a.yearOfDeath - a.yearOfBirth;
+            const nextGuy = b.yearOfDeath - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
+        }
+    });
+    return oldest[0];
 }
-
 module.exports = findTheOldest
--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = function(books) {
+    const titles = books.map(books => books.title)
+    return titles;
+    }
 
 module.exports = getTheTitles;
